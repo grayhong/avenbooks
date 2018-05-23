e9c39a627643ea954190b5283621d30aa1742f09
Youngkyu <yk.hong@kaist.ac.kr>
2018-05-23 22:28:57 +0900

    tutorial done

--- avenbooks-react/src/components/Board.js
+++ avenbooks-react/src/components/Board.js
@@ -1,9 +1,55 @@
 import React, { Component } from 'react';
+import { withCookies, Cookies } from 'react-cookie';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dd: "dd",
+    }
+  }
+  componentWillMount() {
+    const { cookies } = this.props;
+  }
+  componentDidMount() {
+
+  }
   render() {
-    return <div><p>It works!</p></div>
+    const array = [1,2,3];
+    const items = array.map(e => <div key={e*1002}>{e}</div>);
+    return (
+      <div style={styles.contentStyle}>
+        <div style={styles.subStyle}></div>
+        <div style={styles.subStyle}></div>
+        <div style={styles.subStyle2}></div>
+        {items}
+      </div>
+    )
   }
 }
 
-export default Board;
+const styles={
+  contentStyle: {
+    height: '30vh',
+    width: '50vw',
+    backgroundColor: 'blue',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  },
+  subStyle: {
+    backgroundColor: 'white',
+    height: '10vh',
+    width: '10vw',
+
+  },
+  subStyle2: {
+    height: '10vh',
+    width: '10vw',
+
+  }
+}
+
+export default withCookies(Board);
