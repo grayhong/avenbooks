99c3f5c505612e2bc4b3996f300fe48d03a585c0
Kang HyunWoo <khw7147@kaist.ac.kr>
2018-05-27 16:59:25 +0900

    Change board as card not table

--- avenbooks-react/src/components/Board.js
+++ avenbooks-react/src/components/Board.js
@@ -9,62 +9,74 @@ class Board extends Component {
     super(props);
     this.state = {
       input: '',
-      entryState: 'out',
       data: [
         {
           imageSrc: {Logo},
           bookName: 'Introduction to DB',
-          subjectName: 'CS360',
-          cost: '20000',
+          subjectName: 'Intro to DB',
+          subjectID: 'CS360',
+          author: 'Hyun Sun Ju',
+          cost: '2000',
         },
         {
           imageSrc: {Logo},
-          bookName: 'Programing Principles',
-          subjectName: 'CS220',
-          cost: '30000',
+          bookName: 'Structure and Interpretation of Computer Program',
+          subjectName: 'Programming Principles',
+          subjectID: 'CS220',
+          author: 'Hyun Sun Ju',
+          cost: '3000',
         },
         {
           imageSrc: {Logo},
           bookName: 'How to write C code',
-          subjectName: 'CS492',
-          cost: '15000',
+          subjectName: 'Special Lecture on Computer Science',
+          subjectID: 'CS492',
+          author: 'Hyun Sun Ju',
+          cost: '1500',
         },
       ],
       searchData: [
         {
           imageSrc: {Logo},
           bookName: 'Introduction to DB',
-          subjectName: 'CS360',
-          cost: '20000',
+          subjectName: 'Intro to DB',
+          subjectID: 'CS360',
+          author: 'Hyun Sun Ju',
+          cost: '2000',
         },
         {
           imageSrc: {Logo},
-          bookName: 'Programing Principles',
-          subjectName: 'CS220',
-          cost: '30000',
+          bookName: 'Structure and Interpretation of Computer Program',
+          subjectName: 'Programming Principles',
+          subjectID: 'CS220',
+          author: 'Hyun Sun Ju',
+          cost: '3000',
         },
         {
           imageSrc: {Logo},
           bookName: 'How to write C code',
-          subjectName: 'CS492',
-          cost: '15000',
+          subjectName: 'Special Lecture on Computer Science',
+          subjectID: 'CS492',
+          author: 'Hyun Sun Ju',
+          cost: '1500',
         },
       ],
     }
 
     this.detectInput = this.detectInput.bind(this);
     this.afterSearch = this.afterSearch.bind(this);
-    this.changeColor = this.changeColor.bind(this);
-    this.resetColor = this.resetColor.bind(this);
   }
 
   detectInput(e){
     this.setState({ input: e.target.value });
-    console.log(e.target.value);
-    const { data: data, searchData: originData, input } = this.state;
+    const { data: data } = this.state;
+
+    const ignoreCase = String(e.target.value).toLowerCase()
     this.setState({
-      searchData: data.filter(function(item) {
-        return item.bookName.includes(input) || item.subjectName.includes(input);
+      searchData: data.filter(function (item) {
+        return item.bookName.toLowerCase().includes(ignoreCase)
+          || item.subjectName.toLowerCase().includes(ignoreCase)
+          || item.subjectID.toLowerCase().includes(ignoreCase);
       })
     })
   }
@@ -78,18 +90,6 @@ class Board extends Component {
     })
   }
 
-  changeColor(){
-    this.setState({
-      entryState: 'in'
-    })
-  }
-
-  resetColor(){
-    this.setState({
-      entryState: 'out'
-    })
-  }
-
   componentWillMount() {
     const { cookies } = this.props;
   }
@@ -99,143 +99,41 @@ class Board extends Component {
   render() {
     const entry = this.state.searchData.map(function(item) {
       return (
-        <BoardEntry bookName={item.bookName} subjectName={item.subjectName} cost={item.cost}/>
+        <BoardEntry
+          bookName={item.bookName}
+          subjectName={item.subjectName}
+          subjectID={item.subjectID}
+          cost={item.cost}
+          imgSrc={Logo}
+          author={item.author}
+        />
       )
     });
 
     return (
       <div>
-        <div style={styles.gapTop}>
-        </div>
+        <div style={styles.gapTop}></div>
 
         <Form onSubmit={this.afterSearch}>
           <Form.Input
             icon='search'
             fluid
-            placeholder='What are you looking for?'
+            placeholder='Search with subject name or book title'
             onChange={this.detectInput}
             value={this.state.input}
             style={styles.inputStyle}
           />
         </Form>
 
-        <div style={styles.gapTop}>
-        </div>
+        <div style={styles.gapTop}></div>
 
-        <table style={styles.tableFill}>
-          <thead>
-            <tr style={styles.tr}>
-              <th style={styles.th}>Book title</th>
-              <th style={styles.th}>Subject name</th>
-              <th style={styles.th}>Minimum cost</th>
-            </tr>
-          </thead>
-          <tbody>
-            {entry}
-          </tbody>
-        </table>
+        {entry}
       </div>
     )
   }
 }
 
 const styles={
-  contentStyle: {
-    height: '100vh',
-    width: '70vw',
-    margin : 'auto',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  searchBoxStyle:{
-    width: '100vw',
-    display: 'flex',
-    alignItems: 'center',
-  },
-  searchStyle:{
-    width: '80vw',
-  },
-  hiddenStyle:{
-    height: '5vh',
-  },
-  subjectHidden:{
-    height: '5vh',
-  },
-  textBoxStyle: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-  textStyle :{
-    fontSize: '20px',
-  },
-  tableFill: {
-    backgroundColor: 'white',
-    borderRadius: '3px',
-    borderCollapse: 'collapse',
-    margin: 'auto',
-    padding:'5px',
-    width: '80vw',
-  },
-  th: {
-    color:'#D5DDE5',
-    background: '#1b1e24',
-    borderBottom:'4px solid #9ea7af',
-    borderRight: '1px solid #343a45',
-    fontSize:'23px',
-    fontWeight: '100',
-    padding: '24px',
-    textAlign: 'middle',
-    verticalAlign: 'middle',
-  },
-  tr: {
-    borderTop: '1px solid #C1C3D1',
-    borderBottom: '1px solid #C1C3D1',
-    height: '10vh',
-    color: '#666B85',
-    fontSize: '16px',
-    fontWeight: 'normal',
-    textShadow: '0 1px 1px rgba(256, 256, 256, 0.1)',
-  },
-  in:{
-    borderTop: '1px solid #C1C3D1',
-    borderBottom: '1px solid #C1C3D1',
-    backgroundColor:'#D5DDE5',
-    fontSize: '16px',
-    fontWeight: 'normal',
-    textShadow: '0 1px 1px rgba(256, 256, 256, 0.1)',
-  },
-  out: {
-    borderTop: '1px solid #C1C3D1',
-    borderBottom: '1px solid #C1C3D1',
-    backgroundColor:'white',
-    fontSize: '16px',
-    fontWeight: 'normal',
-    textShadow: '0 1px 1px rgba(256, 256, 256, 0.1)',
-  },
-  td: {
-    background: '#FFFFFF',
-    padding: '20px',
-    textAlign: 'left',
-    verticalAlign: 'middle',
-    fontWeight: '300',
-    fontSize: '18px',
-    textShadow: '-1px -1px 1px rgba(0, 0, 0, 0.1)',
-    borderLeft: '1px solid #C1C3D1',
-    borderRight: '1px solid #C1C3D1',
-  },
-  searchTerm: {
-    height: '50vh',
-    width: '80vw',
-    border: '3px solid #00B4CC',
-    padding: '5px',
-    height: '20px',
-    borderRadius: '5px',
-    outline: 'none',
-    color: '#9DBFAF',
-  },
   gapTop:{
     height: '10vh',
   },
--- avenbooks-react/src/components/BoardEntry.js
+++ avenbooks-react/src/components/BoardEntry.js
@@ -4,60 +4,96 @@ import { withCookies, Cookies } from 'react-cookie';
 class BoardEntry extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      entryState: 'out',
-    };
-    this.changeColor = this.changeColor.bind(this);
-    this.resetColor = this.resetColor.bind(this);
-  }
-
-  changeColor(){
-    this.setState({
-      entryState: 'in'
-    })
-  }
-
-  resetColor(){
-    this.setState({
-      entryState: 'out'
-    })
   }
 
   render(){
     return (
-      <tr style={styles.tr}>
-        <td style={{...styles.td, backgroundColor: this.state.entryState === 'out' ? '#ffffff' : '#D5DDE5',}}
-            onMouseEnter={this.changeColor} onMouseLeave={this.resetColor}>{this.props.bookName}</td>
-        <td style={{...styles.td, backgroundColor: this.state.entryState === 'out' ? '#ffffff' : '#D5DDE5',}}
-            onMouseEnter={this.changeColor} onMouseLeave={this.resetColor}>{this.props.subjectName}</td>
-        <td style={{...styles.td, backgroundColor: this.state.entryState === 'out' ? '#ffffff' : '#D5DDE5',}}
-            onMouseEnter={this.changeColor} onMouseLeave={this.resetColor}>{this.props.cost}</td>
-      </tr>
+      <div style={styles.boxStyle}>
+        <img style={styles.imageStyle} src={this.props.imgSrc} alt="professor" />
+        <div style={styles.contentStyle}>
+          <h3 style={styles.subjectNameStyle}>{this.props.subjectName}</h3>
+          <p style={styles.subjectIDStyle}>{this.props.subjectID}</p>
+          <p style={styles.bookNameStyle}>{this.props.bookName}</p>
+          <p style={styles.authorNameStyle}>{"by " + this.props.author}</p>
+          <p style={styles.priceStyle}>{"Price"}</p>
+          <p style={styles.costStyle}>{this.props.cost + "$ ~ "}</p>
+        </div>
+      </div>
     )
   }
 }
 
-const styles={
-  tr: {
-    borderTop: '1px solid #C1C3D1',
-    borderBottom: '1px solid #C1C3D1',
-    height: '10vh',
-    color: '#666B85',
-    fontSize: '16px',
-    fontWeight: 'normal',
-    textShadow: '0 1px 1px rgba(256, 256, 256, 0.1)',
-  },
-  td: {
-    background: '#FFFFFF',
-    padding: '20px',
+const styles = {
+  boxStyle: {
+    border: '0.1rem solid #EDEDED',
+    borderRadius: '0.1rem',
+    width: '80%',
+    height: '18rem',
+    margin: 'auto',
+    marginBottom: '4rem',
     textAlign: 'left',
-    verticalAlign: 'middle',
-    fontWeight: '300',
-    fontSize: '18px',
-    textShadow: '-1px -1px 1px rgba(0, 0, 0, 0.1)',
-    borderLeft: '1px solid #C1C3D1',
-    borderRight: '1px solid #C1C3D1',
+    verticalAlign: 'top',
   },
-}
+  contentStyle: {
+    display: 'inline-block',
+    verticalAlign: 'top',
+    margin: '0.8rem',
+  },
+  imageStyle: {
+    width: '30%',
+    height: '18rem',
+    display: 'inline-block',
+  },
+  subjectNameStyle: {
+    margin: '0.2rem',
+    fontSize: '1.5rem',
+    wordWrap: 'break-word',
+    whiteSpace: 'initial',
+    width: '45rem',
+  },
+  subjectIDStyle: {
+    margin: '0.2rem',
+    marginTop: '0.3rem',
+    fontSize: '0.85rem',
+    color: '#666B85',
+  },
+  bookNameStyle: {
+    margin: '0.2rem',
+    marginTop: '2.5rem',
+    fontSize: '1.15rem',
+    wordWrap: 'break-word',
+    whiteSpace: 'initial',
+    width: '45rem',
+  },
+  authorNameStyle: {
+    margin: '0.2rem',
+    marginTop: '0.3rem',
+    fontSize: '0.85rem',
+    wordWrap: 'break-word',
+    whiteSpace: 'initial',
+    width: '45rem',
+  },
+  priceStyle:{
+    margin: '0.2rem',
+    marginTop: '2.5rem',
+    fontSize: '1.15rem',
+    wordWrap: 'break-word',
+    whiteSpace: 'initial',
+    width: '45rem',
+    textDecoration: 'underline',
+  },
+  costStyle:{
+    margin: '0.2rem',
+    marginTop: '0.3rem',
+    fontSize: '1.15rem',
+    wordWrap: 'break-word',
+    whiteSpace: 'initial',
+    width: '45rem',
+  },
+  subjectStyle: {
+    margin: '0.5rem',
+    fontSize: '0.85rem',
+  },
+};
 
 export default withCookies(BoardEntry);
