918b9dd5c052fc1a5a2890d134844e080df1b1d3
Youngkyu <yk.hong@kaist.ac.kr>
2018-05-27 16:02:57 +0900

    use aws

--- avenbooks-react/src/constants.js
+++ avenbooks-react/src/constants.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'http://ec2-13-125-252-219.ap-northeast-2.compute.amazonaws.com/api/';
-export const BASE_URL = 'http://localhost:8000/api/';
+export const BASE_URL = 'http://ec2-13-125-252-219.ap-northeast-2.compute.amazonaws.com/api/';
+// export const BASE_URL = 'http://localhost:8000/api/';
 export const LOGIN_URL = BASE_URL + 'login';
 export const REGISTER_URL = BASE_URL + 'register';
 
