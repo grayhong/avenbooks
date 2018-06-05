0d014564877b2eda9150a5f3ed5f9ace2a0a7871
Youngkyu <yk.hong@kaist.ac.kr>
2018-06-05 19:55:43 +0900

    db insert changed

--- avenbooks-db/db_insert.sql
+++ avenbooks-db/db_insert.sql
@@ -15,22 +15,22 @@ insert into book(BookName, Author, CurrentEdition) values('Computer Networking: 
 insert into book(BookName, Author, CurrentEdition) values('Programming Languages: Application and Interpretation', 'Shriram Krishnamurthi',  2);
 insert into book(BookName, Author, CurrentEdition) values('Semiconductor Physics and Devices', 'Donald A. Neamen',  4);
 insert into book(BookName, Author, CurrentEdition) values('The C Programming Language', 'Brian Kernighan',  2);
-insert into book(BookName, Author, CurrentEdition) values('Fundamentals of Electric Circuits', 'Charles K Alexander',  6);
-insert into book(BookName, Author, CurrentEdition) values('Principles of Quantum Mechanics', 'R. Shankar', 'PH301', 2);
-insert into book(BookName, Author, CurrentEdition) values('Introduction to Probability and Statistics for Engineers and Scientists', 'Sheldon M. Ross', 'MAS250', 5);
-insert into book(BookName, Author, CurrentEdition) values('Signals and Systems', 'Alan V. Oppenheim', 'EE202', 2);
+insert into book(BookName, Author, CurrentEdition) values('Fundamentals of Electric Circuits', 'Charles K Alexander', 6);
+insert into book(BookName, Author, CurrentEdition) values('Introduction to Probability and Statistics for Engineers and Scientists', 'Sheldon M. Ross', 5);
+insert into book(BookName, Author, CurrentEdition) values('Signals and Systems', 'Alan V. Oppenheim', 2);
+insert into book(BookName, Author, CurrentEdition) values('Linear Algebra', 'Hoffman and Kunz Prentice Hall', 2);
 
-insert into course values('CS360', 'Introduction to Database', 'SoonJoo Hyun', 'SoC');
-insert into course values('CS330', 'Operating System and Lab', 'Junehwa Song', 'SoC');
-insert into course values('CS341', 'Introduction to Network', 'Sung-Ju Lee', 'SoC');
-insert into course values('CS311', 'Computer Architecture', 'Hyunsoo Yoon', 'SoC');
-insert into course values('CS320', 'Programming Language', 'Sukyoung Ryu', 'SoC');
-insert into course values('EE209', 'Programming Structure for Electrical Engineering', 'KyoungSoo Park', 'EE');
-insert into course values('EE201', 'Circuit Theory', 'SeongHwan Cho', 'EE');
-insert into course values('EE211', 'Introduction to Physical Electronic', 'Kyounghoon Yang', 'EE');
-insert into course values('EE202', 'Signals and Systems', 'Jungwoo Choi', 'EE');
-insert into course values('PH301', 'Quantum Mechanics', 'Yeongkwan Kim', 'PHY');
-insert into course values('MAS250', 'Probability and Statistics', 'Ganguk Hwang', 'MAS');
+insert into course values('CS330', 'Operating System and Lab', 'Junehwa Song', 'SoC', 0);
+insert into course values('CS360', 'Introduction to Database', 'SoonJoo Hyun', 'SoC', 1);
+insert into course values('CS311', 'Computer Architecture', 'Hyunsoo Yoon', 'SoC', 2);
+insert into course values('CS341', 'Introduction to Network', 'Sung-Ju Lee', 'SoC', 3);
+insert into course values('CS320', 'Programming Language', 'Sukyoung Ryu', 'SoC', 4);
+insert into course values('EE211', 'Introduction to Physical Electronic', 'Kyounghoon Yang', 'EE', 5);
+insert into course values('EE209', 'Programming Structure for Electrical Engineering', 'KyoungSoo Park', 'EE', 6);
+insert into course values('EE201', 'Circuit Theory', 'SeongHwan Cho', 'EE', 7);
+insert into course values('MAS250', 'Probability and Statistics', 'Ganguk Hwang', 'MAS', 8);
+insert into course values('EE202', 'Signals and Systems', 'Jungwoo Choi', 'EE', 9);
+insert into course values('MAS212', 'Linear Algebra', 'Sanggeun Hahn', 'MaS', 10);
 -- insert into course values('CS376', 'Machine Learning', 'Eunho Yang', 'SoC');
 
 
