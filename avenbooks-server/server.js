0015e14a5614cab3dd7e3e1b3b142741807903dd
Youngkyu <yk.hong@kaist.ac.kr>
2018-06-05 20:22:04 +0900

    server static setting

--- avenbooks-server/server.js
+++ avenbooks-server/server.js
@@ -14,7 +14,7 @@ const app = express();
 // const staticPath = path.posix.join(config.assetsPublicPath, config.assetsSubDirectory);
 
 // const staticPath = path.posix.join(__dirname, 'statics');
-// const staticPath = '/static';
+const staticPath = '/static';
 // console.log(staticPath);
 
 /* Middleware settings. */
