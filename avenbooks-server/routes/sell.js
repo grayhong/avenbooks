4d6ce363db7deb13b03bae22b264241f3f4c67f9
Youngkyu <yk.hong@kaist.ac.kr>
2018-06-06 20:15:13 +0900

    seller name on selling

--- avenbooks-server/routes/sell.js
+++ avenbooks-server/routes/sell.js
@@ -14,7 +14,7 @@ router.get('/sell', async (req, res) => {
     sql = `SELECT * FROM BOOK natural join (SELECT * FROM SELLING WHERE SellerID=${parseInt(sellerID)}) as a order by SellingTime`;
   }
   else if (bookID) {
-    sql = `SELECT SellingID, SellerName, BookID, SellerID, Price, SellingTime FROM Student \
+    sql = `SELECT SellingID, Name as SellerName, BookID, SellerID, Price, SellingTime FROM Student \
                   natural join (SELECT * FROM SELLING WHERE BookID=${parseInt(bookID)}) as b ORDER BY Price`;
   }
   else {
