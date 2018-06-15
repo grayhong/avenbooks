bc31fb166411cfe6d664bb13c1a622f49465dc9a
Youngkyu <yk.hong@kaist.ac.kr>
2018-06-15 16:58:10 +0900

    refactored little

--- avenbooks-db/db_init.sql
+++ avenbooks-db/db_init.sql
@@ -4,55 +4,56 @@ USE UNIVERSITY;
 
 #table creation
 CREATE TABLE STUDENT (
-    StudentID integer PRIMARY KEY,
-    Password varchar(30),
-    Name varchar(30),
+    StudentID integer NOT NULL PRIMARY KEY,
+    Password varchar(30) NOT NULL,
+    Name varchar(30) NOT NULL,
     PhoneNumber char(11)
 );
     
 CREATE TABLE DEPARTMENT (
     DepartmentID varchar(3) NOT NULL PRIMARY KEY,
-    DeptName varchar(30),
+    DeptName varchar(30) NOT NULL,
     PhoneNumber varchar(11)
 );
 
 # 과목별 사용중인 책 목록
 CREATE TABLE BOOK (
     BookID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    BookName varchar(100),
+    BookName varchar(100) NOT NULL,
     CurrentEdition integer,
-    Author varchar(30)
+    Author varchar(30) NOT NULL
 );
 
 CREATE TABLE COURSE (
     CourseID varchar(8) NOT NULL PRIMARY KEY,
-    CourseName varchar(40),
-    Professor varchar(30),
+    CourseName varchar(40) NOT NULL,
+    Professor varchar(30) NOT NULL,
     DepartmentID char(3) NOT NULL,
-    BookID integer,
+    BookID integer NOT NULL,
     FOREIGN KEY (DepartmentID) REFERENCES DEPARTMENT(DepartmentID),
     FOREIGN KEY (BookID) REFERENCES BOOK(BookID)
 );
 
 CREATE TABLE SELLING (
     SellingID integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    BookID integer,
+    BookID integer NOT NULL,
     Edition integer,
-    SellerID integer,
-    Price integer,
-    Finished bool,
+    SellerID integer NOT NULL,
+    Price integer NOT NULL,
+    Finished bool NOT NULL,
     SellingTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (BookID) REFERENCES BOOK(BookID),
     FOREIGN KEY (SellerID) REFERENCES STUDENT(StudentID)
 );
 
 CREATE TABLE TRADE (
-    SellingID integer,
-    BuyerID integer,
-    Confirmed bool,
+    SellingID integer NOT NULL,
+    BuyerID integer NOT NULL,
+    Confirmed bool NOT NULL,
     TradeTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT PK_TRADE PRIMARY KEY (SellingID, BuyerID),
     FOREIGN KEY (SellingID) REFERENCES SELLING(SellingID)
                             ON DELETE CASCADE,
     FOREIGN KEY (BuyerID) REFERENCES STUDENT(StudentID)
 );
+
--- avenbooks-db/db_insert_std_only.sql
+++ avenbooks-db/db_insert_std_only.sql
@@ -8,31 +8,42 @@ INSERT INTO Student(StudentID, Password, Name, phoneNumber)
 INSERT INTO Student(StudentID, Password, Name, phoneNumber)
             VALUES(20160022, 20160022, 'Hyunwoo Kang', '01024237147');
                         
-insert into DEPARTMENT values('SoC', 'Computer Science', '0423503502');
-insert into DEPARTMENT values('MAS', 'Mathematical Science', '0423502702');
-insert into DEPARTMENT values('EE', 'Electrical Engineering', '0423503402');
+INSERT INTO DEPARTMENT VALUES('SoC', 'Computer Science', '0423503502');
+INSERT INTO DEPARTMENT VALUES('MAS', 'Mathematical Science', '0423502702');
+INSERT INTO DEPARTMENT VALUES('EE', 'Electrical Engineering', '0423503402');
 
-insert into book(BookName, Author, CurrentEdition) values('Operating System Concepts', 'Abraham Silberschatz', 9);
-insert into book(BookName, Author, CurrentEdition) values('Fundamentals of Database Systems', 'Ramez Elmasri', 7);
-insert into book(BookName, Author, CurrentEdition) values('Computer System Architecture', 'M. Morris Mano',  3);
-insert into book(BookName, Author, CurrentEdition) values('Computer Networking: A Top-Down Approach', 'Jim Kurose',  7);
-insert into book(BookName, Author, CurrentEdition) values('Programming Languages: Application and Interpretation', 'Shriram Krishnamurthi',  2);
-insert into book(BookName, Author, CurrentEdition) values('Semiconductor Physics and Devices', 'Donald A. Neamen',  4);
-insert into book(BookName, Author, CurrentEdition) values('The C Programming Language', 'Brian Kernighan',  2);
-insert into book(BookName, Author, CurrentEdition) values('Fundamentals of Electric Circuits', 'Charles K Alexander', 6);
-insert into book(BookName, Author, CurrentEdition) values('Introduction to Probability and Statistics for Engineers and Scientists', 'Sheldon M. Ross', 5);
-insert into book(BookName, Author, CurrentEdition) values('Signals and Systems', 'Alan V. Oppenheim', 2);
-insert into book(BookName, Author, CurrentEdition) values('Linear Algebra', 'Hoffman and Kunz Prentice Hall', 2);
+INSERT INTO BOOK(BookName, Author, CurrentEdition) 
+  VALUES('Operating System Concepts', 'Abraham Silberschatz', 9);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Fundamentals of Database Systems', 'Ramez Elmasri', 7);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Computer System Architecture', 'M. Morris Mano',  3);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Computer Networking: A Top-Down Approach', 'Jim Kurose',  7);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Programming Languages: Application and Interpretation', 'Shriram Krishnamurthi',  2);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Semiconductor Physics and Devices', 'Donald A. Neamen',  4);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('The C Programming Language', 'Brian Kernighan',  2);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Fundamentals of Electric Circuits', 'Charles K Alexander', 6);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Introduction to Probability and Statistics for Engineers and Scientists', 'Sheldon M. Ross', 5);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Signals and Systems', 'Alan V. Oppenheim', 2);
+INSERT INTO BOOK(BookName, Author, CurrentEdition)
+  VALUES('Linear Algebra', 'Hoffman and Kunz Prentice Hall', 2);
 
-insert into course values('CS330', 'Operating System and Lab', 'Junehwa Song', 'SoC', 1);
-insert into course values('CS360', 'Introduction to Database', 'SoonJoo Hyun', 'SoC', 2);
-insert into course values('CS311', 'Computer Architecture', 'Hyunsoo Yoon', 'SoC', 3);
-insert into course values('CS341', 'Introduction to Network', 'Sung-Ju Lee', 'SoC', 4);
-insert into course values('CS320', 'Programming Language', 'Sukyoung Ryu', 'SoC', 5);
-insert into course values('EE211', 'Introduction to Physical Electronic', 'Kyounghoon Yang', 'EE', 6);
-insert into course values('EE209', 'Programming Structure for Electrical Engineering', 'KyoungSoo Park', 'EE', 7);
-insert into course values('EE201', 'Circuit Theory', 'SeongHwan Cho', 'EE', 8);
-insert into course values('MAS250', 'Probability and Statistics', 'Ganguk Hwang', 'MAS', 9);
-insert into course values('EE202', 'Signals and Systems', 'Jungwoo Choi', 'EE', 10);
-insert into course values('MAS212', 'Linear Algebra', 'Sanggeun Hahn', 'MaS', 11);
+INSERT INTO COURSE VALUES('CS330', 'Operating System and Lab', 'Junehwa Song', 'SoC', 1);
+INSERT INTO COURSE VALUES('CS360', 'Introduction to Database', 'SoonJoo Hyun', 'SoC', 2);
+INSERT INTO COURSE VALUES('CS311', 'Computer Architecture', 'Hyunsoo Yoon', 'SoC', 3);
+INSERT INTO COURSE VALUES('CS341', 'Introduction to Network', 'Sung-Ju Lee', 'SoC', 4);
+INSERT INTO COURSE VALUES('CS320', 'Programming Language', 'Sukyoung Ryu', 'SoC', 5);
+INSERT INTO COURSE VALUES('EE211', 'Introduction to Physical Electronic', 'Kyounghoon Yang', 'EE', 6);
+INSERT INTO COURSE VALUES('EE209', 'Programming Structure for Electrical Engineering', 'KyoungSoo Park', 'EE', 7);
+INSERT INTO COURSE VALUES('EE201', 'Circuit Theory', 'SeongHwan Cho', 'EE', 8);
+INSERT INTO COURSE VALUES('MAS250', 'Probability and Statistics', 'Ganguk Hwang', 'MAS', 9);
+INSERT INTO COURSE VALUES('EE202', 'Signals and Systems', 'Jungwoo Choi', 'EE', 10);
+INSERT INTO COURSE VALUES('MAS212', 'Linear Algebra', 'Sanggeun Hahn', 'MaS', 11);
 
